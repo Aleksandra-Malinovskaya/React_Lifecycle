cc3d5d30fdbdbd22559399d878e321377a97984b
Aleksandra Malinovskaya <amilinovskaa@gmail.com>
2024-03-23 22:54:52 +0300

    second commit

--- src/components/LifecycleComponent_function.js
+++ src/components/LifecycleComponent_function.js
@@ -1,41 +1,60 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
-const LifecycleComponentFunction = () =>{
-    const [count, SetCount] = useState(0);
+const ChildComponent = ({ count }) => {
+  return (
+    <p>{count}</p>
+  )
+};
 
-    const apiRequest = async () =>{
-        const response = await fetch('https://todo-redev.herokuapp.com/api/users', {
-            method: 'GET',
-            headers:
-            {
-                'accept': 'application/json',
-            }
-        })
-        console.log(response.json());
-    }
+const shouldComponentUpdate = (prevProps, nextProps) => {
+  if (nextProps.count % 2 === 1) {
+      return true;
+  }
+  return false;
+};
 
-    useEffect(() => {
-        apiRequest();
-        return () =>{
-            console.log("Элемент будет размонтирован");
+const MemoizedChildComponent = React.memo(ChildComponent, shouldComponentUpdate);
+
+const LifecycleComponentFunction = () => {
+  const [count, setCount] = useState(0);
+
+  const apiRequest = async () => {
+    try {
+      const response = await fetch('https://todo-redev.herokuapp.com/api/users', {
+        method: 'GET',
+        headers:
+        {
+          'accept': 'application/json',
         }
-    },[]);
-
-    const increment = () =>{
-        SetCount(count + 1);
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    useEffect(() =>{
-        console.log(count);
-    },[count]);
+  useEffect(() => {
+    apiRequest();
+    return () => {
+      console.log("Элемент будет размонтирован");
+    }
+  }, []);
 
-    return(
+  const increment = () => {
+    setCount(count + 1);
+  }
+
+  useEffect(() => {
+    console.log(count);
+  }, [count]);
+
+  return (
     <div>
-        <h2>{count}</h2>
-        <button onClick={increment}>Увеличить</button>
+      <MemoizedChildComponent count={count} />
+      <button onClick={increment}>Увеличить</button>
     </div>
-    )
+  )
 }
 
 export default LifecycleComponentFunction;
